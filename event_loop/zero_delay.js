5d8ba1e993e7a9b35f000fda55cf58f86b5691c1
Nikita Zverev <nikitaz@backbase.com>
2021-01-19 15:42:20 +0100

    event loop example: zero_delay

---
+++ event_loop/zero_delay.js
@@ -0,0 +1,11 @@
+﻿(function(){
+  console.log('this is the start');
+  
+  setTimeout(()=>{console.log('this is a message from the callback');})
+
+  console.log('this is just a message');
+
+  setTimeout(()=>{console.log('this is a message from the callback1');},0)
+
+  console.log('this is the end');
+})()
