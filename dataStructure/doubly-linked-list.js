2cabd3a0fbf67ac866c350f1b3a67eaaa0c729e9
Nikita Zverev <nikitaz@backbase.com>
2021-12-27 20:13:18 +0500

    Added `get`

--- dataStructure/doubly-linked-list.js
+++ dataStructure/doubly-linked-list.js
@@ -84,6 +84,42 @@ class DoublyLinkedListNode {
     return this;
   }
 
+  get(id) {
+    if(id === 0) return this.head;
+    if(id === this.length - 1) return this.tail;
+    if(id < 0 || id >= this.length) return undefined;
+
+    const middle = Math.round(this.length/2);
+
+    return (id <= middle)
+      ? this._getFromStart(id)
+      : this._getFromEnd(id);
+  }
+
+  _getFromStart(id){
+    let current = 0;
+    let result = this.head;
+
+    while(current !== id){
+      result = result.next;
+      current++;
+    }
+
+    return result;
+  }
+
+  _getFromEnd(id){
+    let current = this.length-1;
+    let result = this.tail;
+
+    while(current !== id){
+      result = result.prev;
+      current--;
+    }
+
+    return result;
+  }
+
   _resetPointers(){
     this.head = undefined;
     this.tail = undefined;
@@ -92,9 +128,21 @@ class DoublyLinkedListNode {
 
 const list = new DoublyLinkedListNode();
 
-list.push('first').push('second').unshift('zero').push('third');
+// list.push('first').push('second').unshift('zero').push('third');
 
-console.log('List: ', list);
+for(let i=0; i < 10; i++){
+  list.push(i);
+}
+
+console.log(list)
+
+
+console.log(list.get(0).value);
+console.log(list.get(3).value);
+console.log(list.get(5).value);
+console.log(list.get(7).value);
+console.log(list.get(9).value)
+console.log(list.get(10));
 
 // console.log('Pop: ', list.pop(), list)
 // console.log('Shift: ', list.shift(), list)
