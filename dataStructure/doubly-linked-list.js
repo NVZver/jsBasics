c5b0d808f3026c53bf4022c0229ed5e3a70ffb26
Nikita Zverev <nikitaz@backbase.com>
2021-12-27 16:34:39 +0500

    added `push`

--- dataStructure/doubly-linked-list.js
+++ dataStructure/doubly-linked-list.js
@@ -13,6 +13,28 @@ class DoublyLinkedListNode {
     this.tail = undefined;
     this.length = 0;
   }
+
+  push(value){
+
+    const node = new Node(value);
+
+    if(this.length === 0){
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+
+    this.length++;
+
+    return this;
+  }
 }
 
 const list = new DoublyLinkedListNode();
+
+list.push('first').push('second').push('third');
+
+console.log(list);
