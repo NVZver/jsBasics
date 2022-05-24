62d1963560da32b2d119fddb276ffed5047e0042
Nikita Zverev <nikita.zverev@fedex.com>
2022-05-24 16:36:38 +0200

    added minBinaryHeap

--- dataStructure/heap/binary-heaps.js
+++ dataStructure/heap/binary-heaps.js
@@ -1,8 +1,29 @@
-class MaxBinaryHeap {
+class BinaryHeapBase {
   values;
   constructor(values = []) {
     this.values = values;
   }
+  swapValues(id1, id2) {
+    const temp = this.values[id1];
+    this.values[id1] = this.values[id2];
+    this.values[id2] = temp;
+  }
+  getParent(id) {
+    return Math.floor((id - 1) / 2);
+  }
+  getChildLeft(id) {
+    return id * 2 + 1;
+  }
+  getChildRight(id) {
+    return id * 2 + 2;
+  }
+}
+
+class MaxBinaryHeap extends BinaryHeapBase {
+  constructor(values = []) {
+    super(values);
+  }
+
   insert(value) {
     this.values.push(value);
     return this.bubbleUp();
@@ -48,24 +69,81 @@ class MaxBinaryHeap {
     }
   }
 
-  swapValues(id1, id2) {
-    const temp = this.values[id1];
-    this.values[id1] = this.values[id2];
-    this.values[id2] = temp;
+}
+
+class Node {
+  value;
+  priority;
+  constructor(value, priority) {
+    this.value = value;
+    this.priority = priority;
+  }
+}
+/**
+ * MinBinaryHeap
+ */
+class PriorityQueue extends BinaryHeapBase {
+  constructor(values = []) {
+    super(values);
   }
 
-  getParent(id) {
-    return Math.floor((id - 1) / 2);
+  enqueue(value, priority) {
+    const node = new Node(value, priority);
+    this.values.push(node);
+    this.bubbleUp();
   }
-  getChildLeft(id) {
-    return id * 2 + 1;
+
+  dequeue() {
+    if (this.values.length === 1) return this.values.pop();
+    const root = this.values[0];
+    this.values[0] = this.values.pop();
+
+    this.bubbleDown();
+
+    return root;
   }
-  getChildRight(id) {
-    return id * 2 + 2;
+
+  bubbleDown(id = 0) {
+    const val = this.values[id];
+    const leftId = this.getChildLeft(id);
+    const leftVal = this.values[leftId] || new Node('Infinity', Infinity);
+    const rightId = this.getChildRight(id);
+    const rightVal = this.values[rightId] || new Node('Infinity', Infinity);
+
+    console.log(val, leftVal, rightVal);
+
+    if (val.priority < leftVal.priority && val.priority < rightVal.priority) return;
+
+    if (leftVal.priority < rightVal.priority) {
+      this.swapValues(id, leftId);
+      id = leftId;
+    } else {
+      this.swapValues(id, rightId);
+      id = rightId;
+    }
+    this.bubbleDown(id);
+  }
+
+  bubbleUp(id = this.values.length - 1) {
+    const val = this.values[id];
+    const parentId = this.getParent(id);
+    const parent = this.values[parentId];
+    console.log(val, parent);
+    if (id === 0 || val.priority > parent.priority) return;
+    this.swapValues(id, parentId);
+    this.bubbleUp(parentId);
   }
 }
 
-const heap = new MaxBinaryHeap([40, 35, 25, 34, 23, 16, 20]);
+// const heap = new MaxBinaryHeap([40, 35, 25, 34, 23, 16, 20]);
 // heap.insert(35)
-console.log(heap.extractMax());
-console.log(heap.values);
+// console.log(heap.extractMax());
+// console.log(heap.values);
+
+const priorities = [1, 3, 5, 12, 33, 8, 10];
+const nodes = priorities.map(item => new Node(item, item));
+const queue = new PriorityQueue(nodes);
+queue.enqueue('bla', 0);
+console.log(queue.values.map(item => item.priority));
+queue.dequeue();
+console.log(queue.values.map(item => item.priority));
