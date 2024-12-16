f440283980ec50873dd17f3bab2ad8f38cfc59b8
Nikita Zverev <nv.zver@gmail.com>
2024-12-16 21:36:10 +0000

    Added simple testing tools

---
+++ 2025/simple-testing.js
@@ -0,0 +1,29 @@
+function describe(text, cb){
+    console.log(text);
+    cb();
+}
+
+function test(text, cb){
+    console.log(text);
+    try{
+        cb();
+        console.log(`✅ Passed`);
+    } catch(e){
+        console.log(`❌ Failed`);
+        console.trace(e);
+    }
+}
+
+function expect(value){
+    return {
+        value,
+        toBe: function(testValue){
+            if(value !== testValue){
+                throw new Error(`Expected ${value} to be ${testValue}`);
+            }
+        }
+    }
+}
+
+module.exports = {describe, test, expect};
+
